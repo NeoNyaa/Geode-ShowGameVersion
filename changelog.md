4c6ab59c3bf07dab112f0b5c6d4d6894412d702d
NeoNyaa <56982408+NeoNyaa@users.noreply.github.com>
2025-11-29 17:54:17 +0000

    v1.3.0 - Rewrote most of the code to make adding features easier - Added Cocos2d version number as a number - Changed the styling of the version numbers 	- Version Numbers are seperated with either ` | ` or a newline - Added a license (GNU GPLv3)

--- changelog.md
+++ changelog.md
@@ -1,3 +1,14 @@
+# v1.3.0
+- Rewrote most of the code to make adding features easier
+- Added Cocos2d version number as a number
+- Changed the styling of the version numbers
+	- Version Numbers are seperated with either ` | ` or a newline
+- Added a license (GNU GPLv3)
+
+# v1.2.0
+- Added option to hide GD and/or Geode version
+- Added option to display Geode version on a new line
+
 # v1.1.1
 - Added back support for MacOS
 
@@ -6,7 +17,7 @@
 
 # v1.0.7
 - Dropped support for apple devices
-  - I no longer have a reliable way to test the mod for these platforms
+	- I no longer have a reliable way to test the mod for these platforms
 
 # v1.0.6
 - Improved the v + versionCode concat code
@@ -20,7 +31,7 @@
 # v1.0.3
 - Added links to the homepage and source, both of which point to this repo.
 - Added a support link because geode doesn't actually disable the support button when the support.md file is missing.
-  - As a result I may as well put my PayPal link there, I guess.
+	- As a result I may as well put my PayPal link there, I guess.
 
 # v1.0.2
 - Initial Release
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,83 @@
+#include <unordered_map>
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
 
+struct labelPosition {
+	float posX, posY;
+	float anchorX, anchorY;
+	CCTextAlignment align;
+};
+
 class $modify(MenuLayer) {
-  bool init() {
-    if (!MenuLayer::init()) return false;
+	bool init() {
+		if (!MenuLayer::init()) return false;
 
-    auto confPos = Mod::get()->getSettingValue<std::string>("version-position");
-    auto confOpacity = Mod::get()->getSettingValue<double>("version-opacity");
-    auto confDebug = Mod::get()->getSettingValue<bool>("debug-enabled");
+		// Config
+		bool confGDVersion = Mod::get()->getSettingValue<bool>("conf-showGDVersion");
+		bool confGeodeVersion = Mod::get()->getSettingValue<bool>("conf-showGeodeVersion");
+		bool confCocosVersion = Mod::get()->getSettingValue<bool>("conf-showCocos2dVersion");
+		bool confShowVersionsOnNewLine = Mod::get()->getSettingValue<bool>("conf-versionsOnNewLines");
+		std::string confVersionsPosition = Mod::get()->getSettingValue<std::string>("conf-versionsPosition");
+		double confVersionAlpha = Mod::get()->getSettingValue<double>("conf-versionsOpacity");
+		bool confDebugEnabled = Mod::get()->getSettingValue<bool>("conf-debugLoggingEnabled");
 
-    // Got some help in DMs with this, cpp really isnt that intuitive to me.
-    auto curGDVer = Loader::get()->getGameVersion();
-    auto curGeodeVer = Loader::get()->getVersion().toNonVString();
-    auto labelString = "GD: v" + curGDVer + " (Geode: v" + curGeodeVer + ")";
-    auto labelItem = CCLabelBMFont::create(labelString.c_str(), "bigFont.fnt");
+		// Vars
+		float menuHeight = getContentHeight() - 2;
+		float menuWidth = getContentWidth() - 2;
+		std::string splitCharacter = (confShowVersionsOnNewLine) ? "\n" : " | ";
+		std::string versionStrings[3];
+		std::string labelString;
 
-    auto menuHeight = getContentHeight();
-    auto menuWidth = getContentWidth();
+		// Get versions
+		if (confGDVersion) {
+			versionStrings[0] = std::string("GD: v") + Loader::get()->getGameVersion();
+		}
+		if (confGeodeVersion) {
+			versionStrings[1] = std::string("Geode: v") + Loader::get()->getVersion().toNonVString();
+		}
+		if (confCocosVersion) {
+			versionStrings[2] = std::string("Cocos2d: v") + cocos2d::cocos2dVersion();
+		}
 
-    // Here for making sure I actually got what values I wanted
-    if (confDebug == true) {
-      log::info("Position: {}", confPos);
-      log::info("Main Menu Height: {}", menuHeight);
-      log::info("Main Menu Width: {}", menuWidth);
-      log::info("Opacity: {}", confOpacity);
-      log::info("Game Version: {}", curGDVer);
-      log::info("Geode Version: {}", curGeodeVer);
-    }
+		// Concatenate into single string with seperators between enabled valued
+		for (auto &verString : versionStrings) {
+			if (!verString.empty()) {
+				if (!labelString.empty()) {
+					labelString += splitCharacter;
+				}
+				labelString += verString;
+			}
+		}
 
-    // There is probably a better way of implementing this but it works and doesnt look that bad (at least to me)
-    if (confPos == "Top Left") {
-      labelItem->setPosition(0, menuHeight);
-      labelItem->setAnchorPoint({-0.05f, 1.05f});
-    }
-    if (confPos == "Top Right") {
-      labelItem->setPosition(menuWidth, menuHeight);
-      labelItem->setAnchorPoint({1.05f, 1.05f});
-    }
-    if (confPos == "Bottom Left") {
-      labelItem->setPosition(0, 0);
-      labelItem->setAnchorPoint({-0.05f, -0.2f});
-    }
-    if (confPos == "Bottom Right") {
-      labelItem->setPosition(menuWidth, 0);
-      labelItem->setAnchorPoint({1.05f, -0.2f});
-    }
+		// Map of lable positions
+		std::unordered_map<std::string, labelPosition> layouts = {
+				{"Top Left", {2, menuHeight, 0, 1, kCCTextAlignmentLeft}},
+				{"Top Right", {menuWidth, menuHeight, 1, 1, kCCTextAlignmentRight}},
+				{"Bottom Left", {2, 2, 0, 0, kCCTextAlignmentLeft}},
+				{"Bottom Right", {menuWidth, 2, 1, 0, kCCTextAlignmentRight}},
+		};
 
-    labelItem->setScale(0.25f);
-    labelItem->setOpacity(confOpacity);
-    labelItem->setID("game-version-label");
-    this->addChild(labelItem);
-    return true;
-  }
+		// Make, position, align, style, then add the label
+		auto labelItem = CCLabelBMFont::create(labelString.c_str(), "bigFont.fnt");
+		auto labelOpts = layouts[confVersionsPosition];
+		labelItem->setPosition(labelOpts.posX, labelOpts.posY);
+		labelItem->setAnchorPoint({labelOpts.anchorX, labelOpts.anchorY});
+		labelItem->setAlignment(labelOpts.align);
+		labelItem->setScale(0.25f);
+		labelItem->setOpacity(confVersionAlpha);
+		labelItem->setID("client-versions-label");
+		this->addChild(labelItem);
+
+		// Print values to console
+		if (confDebugEnabled) {
+			log::info("GD Version: {}", versionStrings[0]);
+			log::info("Geode Version: {}", versionStrings[1]);
+			log::info("Cocos Version: {}", versionStrings[2]);
+			log::info("Joined Versions String: {}", labelString);
+		}
+
+		return true;
+	}
 };
